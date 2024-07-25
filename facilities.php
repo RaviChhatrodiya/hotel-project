0bb026c95b7e60e7fcf59c50db900e2178377665
RaviChhatrodiya <trendcart04@gmail.com>
2024-07-25 22:40:50 +0530

    Add files via upload

---
+++ facilities.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hotel - FACILITIES</title>
+   <?php require('inc/links.php')?>
+   <style>
+    .pop:hover{
+      border-top-color: var(--teal) !important;
+      transform: scale(1.03);
+      transition: all 0.3s;
+    }
+   </style>
+  </head>
+  <body class="bg-light">
+
+  <?php require('inc/header.php') ?>
+
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Aliquid quibusdam odio ullam molestias voluptate est,<br> 
+      quae reprehenderit veniam libero magnam dolore pariatur minus ab rerum architecto, 
+      et officia non amet?
+    </p>
+  </div>
+  <div class="cointener">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop ">
+          <div class="d-flex align-items-center mb-2">
+            <img src="images/facilities/wifi.svg" width="40px">
+            <h5 class="m-0 ms-3">wifi</h5>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquam quo fugiat aspernatur tempora delectus! Exercitationem!
+         </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop ">
+          <div class="d-flex align-items-center mb-2">
+            <img src="images/facilities/wifi.svg" width="40px">
+            <h5 class="m-0 ms-3">wifi</h5>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquam quo fugiat aspernatur tempora delectus! Exercitationem!
+         </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop ">
+          <div class="d-flex align-items-center mb-2">
+            <img src="images/facilities/wifi.svg" width="40px">
+            <h5 class="m-0 ms-3">wifi</h5>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquam quo fugiat aspernatur tempora delectus! Exercitationem!
+         </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop ">
+          <div class="d-flex align-items-center mb-2">
+            <img src="images/facilities/wifi.svg" width="40px">
+            <h5 class="m-0 ms-3">wifi</h5>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquam quo fugiat aspernatur tempora delectus! Exercitationem!
+         </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop ">
+          <div class="d-flex align-items-center mb-2">
+            <img src="images/facilities/wifi.svg" width="40px">
+            <h5 class="m-0 ms-3">wifi</h5>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquam quo fugiat aspernatur tempora delectus! Exercitationem!
+         </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop ">
+          <div class="d-flex align-items-center mb-2">
+            <img src="images/facilities/wifi.svg" width="40px">
+            <h5 class="m-0 ms-3">wifi</h5>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquam quo fugiat aspernatur tempora delectus! Exercitationem!
+         </p>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+   <?php require('inc/footer.php')?>
+
+</body>
+</html>
