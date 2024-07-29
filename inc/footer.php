475ee54eab2f6f87916ef080f129df05aab4d43c
RaviChhatrodiya <trendcart04@gmail.com>
2024-07-29 17:26:54 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,42 @@
+<div class="container-fluid bg-white mt-5">
+
+          <div class="row">
+            <div class="col-lg-4">
+              <h3 class="h-font fw-bold fs-3 mb-2">Empire Hotel</h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                 Nulla quae nemo corrupti ut quia voluptatem cumque sapiente neque expedita,
+                 id vero aliquid inventore, maxime illo ipsum magni consequuntur. Possimus, deleniti?
+              </p>
+            </div>
+            <div class="col-lg-4">
+              <h5 class="mb-3">Links</Link></h5>
+              <a href="#" class="d-line-block mb-2 text-dark text-decoration-none">Home</a><br>
+              <a href="#" class="d-line-block mb-2 text-dark text-decoration-none">Room</a><br>
+              <a href="#" class="d-line-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+              <a href="#" class="d-line-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+              <a href="#" class="d-line-block mb-2 text-dark text-decoration-none">About us</a>
+
+             
+              </div>
+              <div class="col-lg-4">
+              <h5 class="mb-3">Follow us</h5>
+              <a href="#" class="d-inline-block mb-3">
+                <span class="badge bg-light text-dark text-decoration-none fs-6 p-2">
+                  <i class="bi bi-twitter me-1"></i> Twitter </a>
+                  <br>
+                  <a href="#" class="d-inline-block mb-3">
+                <span class="badge bg-light text-dark text-decoration-none fs-6 p-2">
+                  <i class="bi bi-facebook me-1"></i>facebook</a>
+                  <br>
+                  <a href="#" class="d-inline-block mb-3">
+                <span class="badge bg-light text-dark text-decoration-none fs-6 p-2">
+                  <i class="bi bi-instagram me-1"></i>instagram</a>
+              </div>
+          </div>
+        </div>
+
+ <h6 class="text-center bg-dark text-white p-3 m-0"> Designed and Developed by ravi and vikram</h6>
+       
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
