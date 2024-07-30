ad440461914db7be219ea45ccac68a3b24ef10ef
RaviChhatrodiya <trendcart04@gmail.com>
2024-07-30 08:57:30 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,9 @@
-<?php require ('inc/db_config.php') ?>
+<?php 
+require ('inc/essentials.php');
+require ('inc/db_config.php');
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,6 +48,19 @@
         $frm_data = filteration($_POST);
 
         $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
+        $values = [$frm_data['admin_name'], $frm_data['admin_pass']];
+
+        $res = select($query,$values,"ss");
+       if($res->num_rows==1){
+        $row = mysqli_fetch_assoc($res);
+        session_start();
+        $_SESSION['adminLogin'] = true;
+        $_SESSION['adminid'] = $row['sr_no'];
+        redirect('dashboard.php');
+       }
+       else{
+       alert('error','Login failed -Invalid Credentials!');
+       }
     }
 
     ?>
