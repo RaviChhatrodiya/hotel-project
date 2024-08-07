f0067cea67ce2105094e53c196f777d312a0df33
RaviChhatrodiya <trendcart04@gmail.com>
2024-08-07 19:19:16 +0530

    Update db_config.php

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -3,24 +3,20 @@ $hname = 'localhost';
 $uname = 'root';
 $pass = '';
 $db = 'myhotel';
-
 $con = mysqli_connect($hname, $uname, $pass, $db);
-
 if (!$con) {
   die("cannot connect to database" . mysqli_connect_error());
 }
-
 function filteration($data)
 {
   foreach ($data as $key => $value) {
     $data[$key] = trim($value);
-    $data[$key] = stripslashes($value);
+    $data[$key] = stripcslashes($value);
     $data[$key] = htmlspecialchars($value);
     $data[$key] = strip_tags($value);
   }
   return $data;
 }
-
 function select($sql, $values, $datatypes)
 {
   $con = $GLOBALS['con'];
@@ -30,15 +26,30 @@ function select($sql, $values, $datatypes)
       $res = mysqli_stmt_get_result($stmt);
       mysqli_stmt_close($stmt);
       return $res;
-    } 
-    else {
+    } else {
       mysqli_stmt_close($stmt);
       die("query cannot be executed - select");
     }
-  
-  } 
-  else {
+  } else {
     die("query cannot be prepared - select");
+  }
+}
+
+function update($sql, $values, $datatypes)
+{
+  $con = $GLOBALS['con'];
+  if ($stmt = mysqli_prepare($con, $sql)) {
+    mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+    if (mysqli_stmt_execute($stmt)) {
+      $res = mysqli_stmt_affected_rows($stmt);
+      mysqli_stmt_close($stmt);
+      return $res;
+    } else {
+      mysqli_stmt_close($stmt);
+      die("query cannot be executed - Update");
+    }
+  } else {
+    die("query cannot be prepared - update");
   }
 }
 
