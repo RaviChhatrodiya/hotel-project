3ac4fdcb5ef75babaa772610d35f4c472e49c088
RaviChhatrodiya <trendcart04@gmail.com>
2024-08-09 12:26:35 +0530

    Update setting.php

--- admin/setting.php
+++ admin/setting.php
@@ -44,7 +44,7 @@ adminLogin();
                 <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
                     aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
-                        <form>
+                        <form id="general_s_form">
 
                             <div class="modal-content">
                                 <div class="modal-header">
@@ -52,22 +52,21 @@ adminLogin();
                                 </div>
                                 <div class="modal-body">
                                     <div class="mb-3">
-                                        <label class="form-label">Site Title</label>
+                                        <label class="form-label fw-bold">Site Title</label>
                                         <input type="text" name="site_title" id="site_title_inp"
-                                            class="form-control shdow-none">
+                                            class="form-control shdow-none" required>
                                     </div>
                                     <div class="mb-3">
-                                        <label class="form-label">About Us</label>
+                                        <label class="form-label  fw-bold">About Us</label>
                                         <textarea name="site_about" id="site_about_inp" class="form-control shadow-none"
-                                            rows="6"></textarea>
+                                            rows="6" required></textarea>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button"
                                         onclick="site_title.value = general_data.site_title, site_about.value = general_data.site_about"
                                         class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="button" onclick="upd_general(site_title.value,site_about.value)"
-                                        class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
                                 </div>
                             </div>
                         </form>
@@ -82,7 +81,8 @@ adminLogin();
                             <h5 class="card-title m-0">Shutdown Website</h5>
                             <div class="form-check form-switch">
                                 <form>
-                                  <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox" id="shutdown-toggle">
+                                    <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox"
+                                        id="shutdown-toggle">
                                 </form>
                             </div>
                         </div>
@@ -91,6 +91,48 @@ adminLogin();
                         </p>
                     </div>
                 </div>
+                <!--  contact detail section -->
+
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Contacts Settings</h5>
+                            <button type="button" class="btn btn-dark shadow-none ntm-sm" data-bs-toggle="modal"
+                                data-bs-target="#contacts-s">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </button>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Address</h6>
+                                    <p class="card-text" id="address"></p>
+                                </div>
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Google Map</h6>
+                                    <p class="card-text" id="gmap"></p>
+                                </div>
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Phone Numbers</h6>
+                                    <p class="card-text mb-1">
+                                        <i class="bi bi-telephone-fill"></i>
+                                        <span id="pn1"></span>
+                                    </p>
+                                    <p class="card-text">
+                                        <i class="bi bi-telephone-fill"></i>
+                                        <span id="pn2"></span>
+                                    </p>
+                                </div>
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">E-mail</h6>
+                                    <p class="card-text" id="email"></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
 
             </div>
         </div>
@@ -102,13 +144,17 @@ adminLogin();
 
         let general_data;
 
+        let general_s_form = document.getElementById('general_s_form');
+        let site_title_inp = document.getElementById('site_title_inp');
+        let site_about_inp = document.getElementById('site_about_inp');
+
+
+
         function get_general() {
 
             let site_title = document.getElementById('site_title');
             let site_about = document.getElementById('site_about');
 
-            let site_title_inp = document.getElementById('site_title_inp');
-            let site_about_inp = document.getElementById('site_about_inp');
 
             let shutdown_toggle = document.getElementById('shutdown-toggle')
 
@@ -125,12 +171,11 @@ adminLogin();
                 site_title_inp.value = general_data.site_title;
                 site_about_inp.value = general_data.site_about;
 
-                if(general_data.shutdown == 0){
+                if (general_data.shutdown == 0) {
                     shutdown_toggle.checked = false;
                     shutdown_toggle.value = 0;
                 }
-                else
-                {
+                else {
                     shutdown_toggle.checked = true;
                     shutdown_toggle.value = 1;
                 }
@@ -142,8 +187,12 @@ adminLogin();
         }
 
 
-        function upd_general(site_title_val, site_about_val)
-         {
+        general_s_form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            upd_general(site_title_inp.value, site_about_inp.value);
+        })
+
+        function upd_general(site_title_val, site_about_val) {
             let xhr = new XMLHttpRequest();
             xhr.open("POST", "ajax/settings_crud.php", true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -163,27 +212,26 @@ adminLogin();
                 }
             }
 
-            xhr.send('site_title=' +site_title_val + '&site_about=' + site_about_val + '&upd_general');
+            xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&upd_general');
 
         }
 
-        function upd_shutdown (val){
+        function upd_shutdown(val) {
             let xhr = new XMLHttpRequest();
             xhr.open("POST", "ajax/settings_crud.php", true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-            xhr.onload = function () {              
-                if (this.responseText == 1 && general_data.shutdown==0) {
+            xhr.onload = function () {
+                if (this.responseText == 1 && general_data.shutdown == 0) {
                     alert('success', 'site has been shutdown!');
                 }
-                else 
-                {
+                else {
                     alert('success', ' shutdown mode off!');
                 }
                 get_general();
             }
 
-            xhr.send('upd_shutdown='+val);
+            xhr.send('upd_shutdown=' + val);
 
         }
 
